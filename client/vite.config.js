8b224aba36fc51c7d35131a0b2724404e17c0974
varun vishwakarma <varun@webwithvarun.com>
2024-07-06 15:31:16 +0530

    some changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://propease.onrender.com',
+        target: 'http://localhost:4000',
         secure: false
       },
     },
