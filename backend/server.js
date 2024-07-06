d63cc973d7b42f92e7387050340669178a9aac4c
varun vishwakarma <varun@webwithvarun.com>
2024-07-06 13:47:53 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 })
 
 connectDB().then(() => {
-    app.listen(4000, () => {
+    app.listen(process.env.PORT, () => {
         console.log("Server is running on port 4000!")
     })
 }).catch((error) => {
