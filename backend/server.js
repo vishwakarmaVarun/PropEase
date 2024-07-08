8253492c4a916dabdca960f37548ea9a33e83a3c
varun vishwakarma <varun@webwithvarun.com>
2024-07-08 15:18:39 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.router.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -16,6 +19,11 @@ app.use(cors())
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, './client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./client", "dist", "index.html"))
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
